5834d4a7dd6afd6a89c41e7b588a272777772894
sourav@ion <sourav.ion@gmail.com>
2023-08-12 21:55:56 +0530

    setup the project

--- test.js
+++
@@ -1,8 +0,0 @@
-// const logger  = require('./index');
-// console.log(logger.green('Operation completed successfully.'));
-// console.log(logger.red('An error occurred.'));
-// console.log(logger.blue('Here is some information.'));
-// console.log(logger.cyan('This is bold text.'));
-// console.log(logger.yellow('This is underlined text.'));
-// console.log(logger.magenta('This is underlined text.'));
-// console.log(logger.bgGreen('This is underlined text.'))
